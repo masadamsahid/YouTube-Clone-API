4bee0ae082bd5f962295c471d44d58cfaf08f529
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-07-21 13:01:38 +0700

    set `update` and `delete` user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,36 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import { 
+  deleteUser, 
+  dislike, 
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  update
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+// update user
+router.put("/:id", verifyToken, update);
+
+// delete user
+router.delete("/:id",verifyToken, deleteUser);
+
+// get a user
+router.get("/find/:id", getUser);
+
+// subscribe user
+router.put("/sub/:id",verifyToken, subscribe);
+
+// unsubscribe user
+router.put("/unsub/:id",verifyToken, unsubscribe);
+
+// like video
+router.put("like/:videoId",verifyToken, like);
+
+// dislike video
+router.put("dislike/:videoId",verifyToken, dislike);
 
 export default router;
