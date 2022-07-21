6403f6d08a73c8d27f5f451b07d401c5684c7fc6
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-07-22 06:27:29 +0700

    add comments APIs

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,16 @@
 import express from "express";
-import {  } from "../controllers/comment.js";
+import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { verifyToken } from '../verifyToken.js'
 
 const router = express.Router();
+
+// Add a comment to a video
+router.post("/", verifyToken, addComment);
+
+// Delete a comment from a video
+router.delete("/:id", verifyToken, deleteComment);
+
+// Get comments from a video
+router.get("/:videoId", getComments);
 
 export default router;
